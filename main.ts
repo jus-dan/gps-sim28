475eef80f38e36739a4faa38be9e991956078cae
Danilo Just <92367836+jus-dan@users.noreply.github.com>
2023-04-22 14:09:11 +0200

    Lora integrated

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ function savePosition (cmd: string) {
     serial.writeLine("" + (time))
     serial.writeLine("" + (latitude))
     serial.writeLine("" + (longitude))
-    serial.writeLine("" + (altitude))
+    serial.writeLine(altitude)
 }
 let received_cmd = ""
 let altitude = ""
@@ -15,20 +15,27 @@ let longitude = ""
 let latitude = ""
 let time = ""
 let temp: string[] = []
+IotLoRaNode.SetRegion(region.EU868)
+IotLoRaNode.InitialiseRadioABP(
+"260B0B32",
+"3732CADD3D61809F9978E5983E81BC06",
+"94CB3D750FC6E6314F6F4FC542E8EFFC",
+SpreadingFactors.Seven
+)
+// Anschluss vom GPS (SIM28)
 serial.redirect(
 SerialPin.P1,
-SerialPin.P0,
+SerialPin.P2,
 BaudRate.BaudRate9600
 )
 serial.setRxBufferSize(254)
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
-        `)
+    // Anschluss vom GPS (SIM28)
+    serial.redirect(
+    SerialPin.P1,
+    SerialPin.P2,
+    BaudRate.BaudRate9600
+    )
     received_cmd = "deadbeef"
     while (!(received_cmd.includes("GPGGA,"))) {
         received_cmd = serial.readUntil(serial.delimiters(Delimiters.Dollar))
@@ -36,16 +43,23 @@ basic.forever(function () {
     serial.redirectToUSB()
     serial.writeLine(received_cmd)
     savePosition(received_cmd)
+    // Anschluss vom LoRa-Modul
     serial.redirect(
-    SerialPin.P1,
-    SerialPin.P0,
-    BaudRate.BaudRate9600
+    SerialPin.P14,
+    SerialPin.P15,
+    BaudRate.BaudRate115200
     )
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
+    IotLoRaNode.TemperatureValue(input.temperature(), Channels.One)
+    IotLoRaNode.GPS(
+    11,
+    50,
+    parseFloat(altitude),
+    Channels.One
+    )
+    IotLoRaNode.loraTransmitPayload()
+})
+basic.forever(function () {
+    basic.showString("" + (latitude))
+    basic.showString("" + (longitude))
+    basic.showString(altitude)
 })
